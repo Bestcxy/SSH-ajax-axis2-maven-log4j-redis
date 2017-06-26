3b2018a4efab9251518b68f2f5b60be5ab4d732c
wujie <snailknight@yeah.net>
2017-06-26 23:03:07 +0800

    Socket 服务端和客户端连接测试

---
+++ src/main/java/com/bestcxx/mavenstu/mavenssh/socket/ClientSocketAction.java
@@ -0,0 +1,51 @@
+package com.bestcxx.mavenstu.mavenssh.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * 客户端Socket
+ * @author WuJieJecket
+ *
+ */
+public class ClientSocketAction {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket=new Socket("127.0.0.1",10000 );
+			OutputStream outputStream=socket.getOutputStream();
+			PrintWriter printWriter=new PrintWriter(outputStream);
+			printWriter.write("你好，我是客户端");
+			printWriter.flush();
+			socket.shutdownOutput();
+			
+			//获取服务器端返回的信息
+			//获取服务端输入流
+			InputStream inputStream=socket.getInputStream();
+			byte b[]=new byte[1024];
+			StringBuffer sbf=new StringBuffer();
+			for(int n;(n=inputStream.read(b))!=-1;){
+				sbf.append(new String(b,0,n));				
+			}
+			System.out.println("服务器端反馈进来的信息为："+sbf.toString());
+			socket.shutdownInput();
+			
+			//关闭资源
+			printWriter.close();
+			outputStream.close();
+			
+			inputStream.close();
+			socket.close();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/bestcxx/mavenstu/mavenssh/socket/ServerSocketAction.java
@@ -0,0 +1,59 @@
+package com.bestcxx.mavenstu.mavenssh.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 基于 TCP 协议的
+ * 服务器端的Socket
+ * @author WuJieJecket
+ *
+ */
+public class ServerSocketAction {
+	
+	public static void main(String[] args) {
+		try {
+			//创建服务器端的 Socket
+			@SuppressWarnings("resource")
+			ServerSocket serverSocket=new ServerSocket(10000);
+			
+			//开始监听服务器端的 Socket,一旦客户端调用 socket 就被实例化
+			Socket socket=serverSocket.accept();
+			
+			//获取客户端输入流
+			InputStream inputStream=socket.getInputStream();
+			byte b[]=new byte[1024];
+			StringBuffer sbf=new StringBuffer();
+			for(int n;(n=inputStream.read(b))!=-1;){
+				sbf.append(new String(b,0,n));				
+			}
+			System.out.println("客户端传递进来的信息为："+sbf.toString());
+			socket.shutdownInput();
+			
+			//向客户端反馈信息
+			OutputStream outputStream=socket.getOutputStream();
+			PrintWriter printWriter=new PrintWriter(outputStream);
+			printWriter.write("我是服务端");
+			printWriter.flush();
+			socket.shutdownOutput();
+			
+			//关闭资源
+			
+			inputStream.close();
+			printWriter.close();
+			outputStream.close();
+			
+			socket.close();
+			serverSocket.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
