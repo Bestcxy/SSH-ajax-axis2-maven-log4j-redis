c695690f74a53c1f67e30d9c70cc2d5ece4ec4d6
wujie <snailknight@yeah.net>
2017-07-30 19:34:37 +0800

    Redis 配置完善

--- src/main/java/com/bestcxx/mavenstu/mavenssh/redis/RedisServer.java
+++ src/main/java/com/bestcxx/mavenstu/mavenssh/redis/RedisServer.java
@@ -34,13 +34,21 @@ public class RedisServer {
 			JedisPoolConfig config = new JedisPoolConfig();
 			// 控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
 			// 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-			config.setMaxIdle(500);
+			config.setMaxTotal(500);
 			// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
 			config.setMaxIdle(5);
-			// 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
+			//多长空闲时间之后回收空闲连接
+			config.setMinEvictableIdleTimeMillis(60000);
+			//设置初始化连接数
+			config.setMinIdle(10);
+			// 最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
 			config.setMaxWaitMillis(1000 * 100);
-			// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+			// 在borrow一个jedis实例时,是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
 			config.setTestOnBorrow(true);
+			//在return给pool时，是否提前进行validate操作；
+			config.setTestOnReturn(false);
+			//启动空闲连接的测试
+			config.setTestWhileIdle(false);
 			//pool = new JedisPool(config, "192.168.1.100", 6379);
 			pool = new JedisPool(config, "127.0.0.1", 6379);
 		}
